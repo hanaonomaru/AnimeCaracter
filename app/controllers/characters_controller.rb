22552e2f6b5be362ab53348f0c73231fe64e8c61
hanaonomaru <hanao.n1227@gmail.com>
2021-06-12 21:27:26 +0900

    キャラクターの入力欄を作成しました

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -1,5 +1,21 @@
 class CharactersController < ApplicationController
   def index
     @characters = AnimeCharacter.all
+    @character = AnimeCharacter.new
   end
+
+  def create
+    @character = AnimeCharacter.create!(character_params)
+    if @character.save
+      redirect_to action: :index
+    else
+      render :index
+    end
+  end
+
+  private
+    def character_params
+      params.require(:anime_character).permit(:name, :age, :gender, :voice_acter)
+    end
+  
 end
